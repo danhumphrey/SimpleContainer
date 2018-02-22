982fc4bc0c19b89cedd8304a8f4b03ec6401d9a5
Dan Humphrey <danh2000@gmail.com>
2018-02-22 19:50:45 +1100

    initial implementation

--- src/main/java/com/github/danhumphrey/ioc/SimpleContainer.java
+++ src/main/java/com/github/danhumphrey/ioc/SimpleContainer.java
@@ -1,8 +1,73 @@
 package com.github.danhumphrey.ioc;
 
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * SimpleContainer is a simple Registry used to store and retrieve common
+ * objects by type or identifier. Can be used with Dependency Injection for
+ * sharing dependencies.
+ * 
+ * @author Dan Humphrey
+ *
+ */
 public class SimpleContainer {
 
-	public SimpleContainer() {
+	private static Map<String, Object> entries;
 
+	public SimpleContainer() {
+		if (entries == null) {
+			entries = new HashMap<String, Object>();
+		}
 	}
+
+	/**
+	 * Checks the entries for an entry with a matching type
+	 * 
+	 * @param type the type you want to check
+	 * @return true if a matching entry was found and false otherwise
+	 */
+	public boolean hasEntry(Type type) {
+		return entries.containsKey(type.getTypeName());
+	}
+
+	/**
+	 * Checks the entries for an entry with a matching name
+	 * 
+	 * @param type the type you want to check
+	 * @return true if a matching entry was found and false otherwise
+	 */
+	public boolean hasNamedEntry(String entryName) {
+		return entries.containsKey(entryName);
+	}
+	
+	/**
+	 * Registers an entry in the container. If the container already has an entry of
+	 * the same type it will be overwritten.
+	 * 
+	 * @param entry the entry to store in the container
+	 */
+	public <T> void register(T entry) {
+		entries.put(entry.getClass().getTypeName(), entry);
+	}
+	
+	/**
+	 * Registers an entry in the container with an entry name which allows multiple
+	 * entries of the same type to be registered.
+	 * 
+	 * @param entryName the unique name of the entry
+	 * @param entry the entry to store in the container
+	 */
+	public <T> void register(String entryName, T entry) {
+		entries.put(entryName, entry);
+	}
+	
+	/**
+	 * Removes all entries
+	 */
+	public void removeAll() {
+		entries.clear();
+	}
+
 }
--- src/test/java/com/github/danhumphrey/ioc/TestSimpleContainer.java
+++ src/test/java/com/github/danhumphrey/ioc/TestSimpleContainer.java
@@ -1,7 +1,6 @@
 package com.github.danhumphrey.ioc;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +8,8 @@ import org.junit.jupiter.api.Test;
 
 class TestSimpleContainer {
 
+	SimpleContainer container;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
@@ -19,11 +20,35 @@ class TestSimpleContainer {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		container = new SimpleContainer();
+		container.removeAll();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testHasEntryMethodReturnsFalseWhenNothingRegisteredInContainer() {
+		Assert.assertFalse("Expecting hasEntry to return false when nothing has been registered", container.hasEntry(String.class));
+	}
+	
+	@Test
+	public void testHasEntryMethodReturnsTrueWhenMatchingEntryRegisteredInContainer() {
+		container.register("This is a String");
+		Assert.assertTrue("Expecting hasEntry to return true when matching entry has been registered", container.hasEntry(String.class));
 	}
 
+	@Test
+	public void testHasNamedEntryMethodReturnsFalseWhenNothingRegisteredInContainer() {
+		Assert.assertFalse("Expecting hasNamedEntry to return false when nothing has been registered", container.hasNamedEntry("someRandomName"));
+	}
+	
+	@Test
+	public void testHasNamedEntryMethodReturnsFalseWhenEntryRegisteredWithDifferentName() {
+		container.register("someDifferentName", "This is a String");
+		Assert.assertFalse("Expecting hasNamedEntry to return false when entry has been registered with a different name", container.hasNamedEntry("someRandomName"));
+	}
+	
+	@Test
+	public void testHasNamedEntryMethodReturnsTrueWhenEntryRegisteredWithMatchingName() {
+		container.register("someRandomName", "This is a String");
+		Assert.assertTrue("Expecting hasNamedEntry to return false when entry has been registered with a different name", container.hasNamedEntry("someRandomName"));
+	}
 }
